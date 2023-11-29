39844403b7d29a769ece6aea7cb12e2442d317e9
PascualDomingo <pascodom@gmail.com>
2023-11-29 16:33:42 -0600

    v1

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  //imports: [CommonModule],
   styleUrls: ['./header.component.css'],
   templateUrl: './header.component.html'
   
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { bootstrapApplication } from '@angular/platform-browser';
+//import { bootstrapApplication } from '@angular/platform-browser';
 //import { appConfig } from './app/app.config';
 //import { AppComponent } from './app/app.component';
 
